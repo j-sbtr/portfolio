a5a8c8b5fd2851b9fc888d815f1f7103be7b0f68
Jasmin Sabater <sabaterjasmin10@gmail.com>
2026-05-11 15:10:42 +0800

    Add javascript file

---
+++ style.js
@@ -0,0 +1,13 @@
+//Picture enlargement on certificate
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImg");
+
+function openModal(src) {
+    modal.classList.remove("hidden");
+    modalImg.src = src;
+}
+
+modal.addEventListener("click", () => {
+    modal.classList.add("hidden");
+});
+
