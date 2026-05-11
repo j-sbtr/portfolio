7baccdeb75d36b6683d4939db6a6d22718456a88
Jasmin Sabater <sabaterjasmin10@gmail.com>
2026-05-11 15:50:35 +0800

    Add hamburger function on javascript file

--- style.js
+++ style.js
@@ -11,3 +11,18 @@ modal.addEventListener("click", () => {
     modal.classList.add("hidden");
 });
 
+
+//Hamburger
+function Menu(e){
+    let list = document.querySelector('ul');
+    
+    if(e.name === 'menu'){
+        e.name = "close";
+        list.classList.add('top-[80px]', 'opacity-100');
+        list.classList.remove('pointer-events-none');
+    } else {
+        e.name = "menu";
+        list.classList.remove('top-[80px]', 'opacity-100');
+        list.classList.add('pointer-events-none');
+        }
+}
