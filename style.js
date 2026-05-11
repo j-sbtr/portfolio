99358223a6a8ff7c83dfeb0ad5f1b86d95714040
Jasmin Sabater <sabaterjasmin10@gmail.com>
2026-05-11 21:08:46 +0800

    Update all html files to use 2-space indentation

--- style.js
+++ style.js
@@ -15,14 +15,17 @@ modal.addEventListener("click", () => {
 //Hamburger
 function Menu(e){
     let list = document.querySelector('ul');
-    
-    if(e.name === 'menu'){
-        e.name = "close";
-        list.classList.add('top-[80px]', 'opacity-100');
-        list.classList.remove('pointer-events-none');
-    } else {
-        e.name = "menu";
-        list.classList.remove('top-[80px]', 'opacity-100');
-        list.classList.add('pointer-events-none');
+        if (window.innerWidth < 640) {
+            if(e.name === 'menu'){
+                e.name = "close";
+                list.classList.add('top-[80px]', 'opacity-100');
+                list.classList.remove('pointer-events-none');
+        } else {
+            e.name = "menu";
+            list.classList.remove('top-[80px]', 'opacity-100');
+            list.classList.add('pointer-events-none');
         }
+    }
 }
+
+
